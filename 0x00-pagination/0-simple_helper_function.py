5f04fb4d88f7c75c77c0577360f035503c635e1b
elcodo254 <ellawachaga@gmail.com>
2023-08-21 17:36:36 +0000

    fix pycodestyle error

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,7 +6,7 @@ function to return start and end index corresponding to pagination parameters
 from typing import Tuple
 
 
-def index_range (page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     return a tuple of size two containing a start index and an end index
     corresponding to the range of indexes to return in a list for those
