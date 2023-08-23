eddf5a988e8921ffbb9b7ab538daa09c77607a62
elcodo254 <ellawachaga@gmail.com>
2023-08-23 23:44:00 +0000

    lifo cache replacement

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+LIFO caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    A lifo caching system.
+    Attributes:
+      __init__ - initializes class instance
+      put - method that adds a key/value pair to cache
+      get - method that retrieves a key/value pair from cache
+    """
+
+    def __init__(self):
+        """
+        initialize class instance
+        """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """
+        assign to self.cache_data the item value for the key key.
+        if number of items in cache is higher that BaseCaching.MAX_ITEMS:
+          discard the last item put in cache(LIFO)
+          print DISCARD with the discarded key
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.keys:
+                self.keys.append(key)
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                discard = self.keys.pop(-22)
+                del self.cache_data[discard]
+                print(f'DISCARD: {discard}')
+
+    def get(self, key):
+        """
+        Returns value in 'key' of cache
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
